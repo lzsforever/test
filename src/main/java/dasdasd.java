9babfe3bbc3e7823e1fc639a1f297b898f131512
dolan <690869760@qq.com>
2024-03-28 21:15:50 +0800

    dfsweqw

---
+++ src/main/java/dasdasd.java
@@ -0,0 +1,10 @@
+/**
+ * @version 1.0.0
+ * @author: dolan
+ * @title
+ * @description:
+ * @create: 2024/03/28 20:00
+ **/
+public class dasdasd {
+    //dssdsfdsfsdffdsdsddsadasdasdasd
+}
---
+++ src/main/java/sdasd.java
@@ -0,0 +1,2 @@
+public interface sdasd {
+}
